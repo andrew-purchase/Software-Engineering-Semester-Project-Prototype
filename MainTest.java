e3643fc0966b585a95f59f3eb2c4ec766b8671f0
andrew-purchase <61091050+andrew-purchase@users.noreply.github.com>
2020-02-18 15:01:14 -0600

    Add files via upload

---
+++ MainTest.java
@@ -0,0 +1,12 @@
+package test1;
+
+import java.io.File;
+
+public class MainTest {
+	public static void main(String[] args) throws Exception { 
+		
+		txtFileChooser t1 = new txtFileChooser();
+//		System.out.println(new File(".").getAbsolutePath());
+
+	}
+}
